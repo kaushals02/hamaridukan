45b8aeca5af0b18356c5f68d7167e06203e6b40b
kaushals02 <sharmakaushal089@gmail.com>
2023-11-02 16:31:13 +0530

    vercelhost

--- src/components/Checking/Shipping.js
+++ src/components/Checking/Shipping.js
@@ -26,7 +26,7 @@ export default function Shipping({ email, number }) {
 
   const orderPlaced = () => {
     alert("Your order has been placed! Happy Shopping");
-    window.location.href = "http://localhost:3000/"/*"https://humaridukan.netlify.app/"*/;
+    window.location.href = "https://hamaridukan.vercel.app/"/*"http://localhost:3000/"*/;
   };
 
   return (
